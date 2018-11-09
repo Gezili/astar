84fe0613feb6f63829b413691aa0dc799436f1c8
Gezili <zilig1998@gmail.com>
2018-11-08 23:52:52 -0500

    refactor

--- astar.py
+++ astar.py
@@ -3,8 +3,9 @@ from shapely.geometry.polygon import Polygon
 from math import sqrt
 from matplotlib import collections
 import numpy as np
-
 import matplotlib.pyplot as plt
+
+LEN_GRAPH = 35
 
 class Node:
     
@@ -18,10 +19,9 @@ class Node:
         
 class Edge:
     
-    def __init__(self, StartNode, EndNode, Cost):
+    def __init__(self, StartNode, EndNode):
         self.StartNode = StartNode
         self.EndNode = EndNode
-        self.Cost = Cost
 
 class Graph:
     
@@ -33,8 +33,8 @@ class Graph:
 
 class Shape:
     
-    def __init__ (self, Coordinates):
-        self.Coordinates = Coordinates
+    def __init__ (self):
+        self.Coordinates = []
         
     def add_point(self, Coordinate):
         self.Coordinates.append(Coordinate)
@@ -46,12 +46,12 @@ def inside_polygon(polygon, point):
     polygon = Polygon(polygon)
     point = Point(point)
     return polygon.covers(point)
-    
+
 def inside_region(node):
     
     coordinate = node.Coordinate
     flag = False
-    if 0 <= coordinate[0] <= 34 and 0 <= coordinate[1] <= 34:
+    if 0 <= coordinate[0] <= LEN_GRAPH - 1 and 0 <= coordinate[1] <= LEN_GRAPH - 1:
         flag = True
     
     return flag
@@ -59,18 +59,16 @@ def inside_region(node):
 '''Generate a list of all nodes in the graph'''
 def generate_all_nodes(sparse = False):
     
-    shapes = [s1, s2, s3, s4, s5, s6]
-    
     nodes = []
     
     if sparse:
-        for i in range(35*35):
+        for i in range(LEN_GRAPH*LEN_GRAPH):
             nodes.append(0)
             
-    for i in range(35):
-        for j in range(35):
+    for i in range(LEN_GRAPH):
+        for j in range(LEN_GRAPH):
             
-            node = Node(i*35 + j, (i, j))
+            node = Node(i*LEN_GRAPH + j, (i, j))
             inside_polygon_flag = False
             
             for shape in shapes:
@@ -80,7 +78,7 @@ def generate_all_nodes(sparse = False):
             if not inside_polygon_flag and inside_region(node) and not sparse:
                 nodes.append(node)
             elif not inside_polygon_flag and inside_region(node):
-                nodes[i*35 + j] = node
+                nodes[i*LEN_GRAPH + j] = node
                 
     return nodes
             
@@ -98,9 +96,9 @@ def generate_all_edges(return_nodes = False):
         #Check 4 directions
         check_nodes = [
             node_orig.ID + 1, #north
-            node_orig.ID + 35, #east
-            node_orig.ID + 36, #northeast
-            node_orig.ID + 34, #southeast
+            node_orig.ID + LEN_GRAPH, #east
+            node_orig.ID + LEN_GRAPH + 1, #northeast
+            node_orig.ID + LEN_GRAPH - 1, #southeast
         ]
         
         for i, node_dest in enumerate (check_nodes):
@@ -114,12 +112,13 @@ def generate_all_edges(return_nodes = False):
                 
                 #Hacky! Python has issues with rounding sometimes
                 if dist < sqrt(2) + 0.01:
-                    edge = Edge(node_orig, node_dest, dist)
+                    edge = Edge(node_orig, node_dest)
                     edges.append(edge)
             
     return edges
         
 def visualize_nodes(nodes):
+    
     node_x = [node.Coordinate[0] for node in nodes]
     node_y = [node.Coordinate[1] for node in nodes]
     plt.plot(node_x, node_y, 'ro')
@@ -158,24 +157,39 @@ def construct_graph():
 def generate_graph_dict():
     
     graph_dict = {}
-    
     nodes = generate_all_nodes()
-    edges = generate_all_edges()
+    node_ids = [node.ID for node in nodes]
     
-    for node in nodes:
-        for edge in edges:
-            if edge.StartNode.ID == node.ID:
-                if node.ID not in graph_dict:
-                    graph_dict[node.ID] = [edge.EndNode.ID]
-                else:
-                    graph_dict[node.ID].append(edge.EndNode.ID)
-                    
-            if edge.EndNode.ID == node.ID:
-                if node.ID not in graph_dict:
-                    graph_dict[node.ID] = [edge.StartNode.ID]
-                else:
-                    graph_dict[node.ID].append(edge.StartNode.ID)
-            
+    for node_orig in nodes:
+        
+        #Check all 8 directions
+        check_nodes = [
+            node_orig.ID + 1, #north
+            node_orig.ID + LEN_GRAPH, #east
+            node_orig.ID + LEN_GRAPH + 1, #northeast
+            node_orig.ID + LEN_GRAPH - 1, #southeast
+            node_orig.ID - 1,
+            node_orig.ID - LEN_GRAPH,
+            node_orig.ID - LEN_GRAPH + 1,
+            node_orig.ID - LEN_GRAPH - 1
+        ]
+        
+        for i, node_dest in enumerate (check_nodes):
+            if node_dest in node_ids:
+                
+                node_dest = nodes[node_ids.index(node_dest)]
+                dist = calculate_euclidean_distance(
+                    node_dest.Coordinate,
+                    node_orig.Coordinate
+                )
+                
+                #Hacky! Python has issues with rounding sometimes
+                if dist < sqrt(2) + 0.01:
+                    if node_orig.ID not in graph_dict:
+                        graph_dict[node_orig.ID] = [node_dest.ID]
+                    else:
+                        graph_dict[node_orig.ID].append(node_dest.ID)
+                        
     return graph_dict
     
 def a_star():
@@ -183,6 +197,9 @@ def a_star():
     graph = construct_graph()
     closed_set = []
     open_set = []
+    
+    coord_begin = (2, 2)
+    coord_end = (32, 32)
     
     nodes = generate_all_nodes(sparse = True)
 
@@ -195,21 +212,23 @@ def a_star():
             node.Cost = 100
             node.HeuristicCost = calculate_euclidean_distance(
                 node.Coordinate, 
-                (32, 32) #End_Node Coordinate
+                coord_end #End_Node Coordinate
             )
 
     #This is our beginning node
-    nodes[35*2 + 2].Cost = nodes[35*2 + 2].HeuristicCost
-    node_current = nodes[35*2 + 2]
+    nodes[LEN_GRAPH*coord_begin[0] + coord_begin[1]].Cost =\
+    nodes[LEN_GRAPH*coord_begin[0] + coord_begin[1]].HeuristicCost
+    node_current = nodes[LEN_GRAPH*coord_begin[0] + coord_begin[1]]
     open_set.append(node_current.ID)
          
-    while open_set != []:
+    while open_set:
+        
         #Find the index of the current node from the open set
         node_current_id = open_set[np.argmin(np.array(\
         [nodes[open_set[i]].Cost + nodes[open_set[i]].HeuristicCost\
         for i in range(len(open_set))]))]
         
-        if nodes[node_current_id].Coordinate == (32, 32):
+        if nodes[node_current_id].Coordinate == coord_end:
             
             path = []
             
@@ -225,28 +244,33 @@ def a_star():
         
         node_list = graph.ConnectivityGraph[node_current_id]
         
+        #Node has been visited - let's move on
         for node_id in node_list:
             if node_id in closed_set:
                 continue
             
+            #Update cost
             temp_cost = nodes[node_current_id].Cost +\
             calculate_euclidean_distance(nodes[node_current_id].Coordinate,\
             nodes[node_id].Coordinate)
             
             if node_id not in open_set:
                 open_set.append(node_id)
+                
+            #There is a better path - ignore
             elif temp_cost >= nodes[node_id].Cost:
                 continue
-                
+            
+            #Update costs, origin
             nodes[node_id].Origin = node_current_id
             nodes[node_id].Cost = temp_cost
             nodes[node_id].HeuristicCost = temp_cost + nodes[node_id].HeuristicCost
     
-    return nodes
+    #Check to make sure openset is not null - if it is, then there is NO path      
+    assert open_set
         
 def print_path():
     
-    shapes = [s1, s2, s3, s4, s5, s6]
     edges = generate_all_edges()
     
     line_segs = []
@@ -283,43 +307,45 @@ def print_path():
 if __name__ == '__main__':
     #Declare all the shapes
     
-    s1 = Shape([])
+    s1 = Shape()
     s1.add_point((6, 10))
     s1.add_point((12, 10))
     s1.add_point((12, 4))
     s1.add_point((10, 4))
     s1.add_point((6, 8))
     
-    s2 = Shape([])
+    s2 = Shape()
     s2.add_point((9, 16))
     s2.add_point((9, 20))
     s2.add_point((12, 20))
     s2.add_point((12, 16))
     
-    s3 = Shape([])
+    s3 = Shape()
     s3.add_point((17, 15))
     s3.add_point((17, 11))
     s3.add_point((14, 11))
     s3.add_point((14, 15))
     
-    s4 = Shape([])
+    s4 = Shape()
     s4.add_point((20, 6))
     s4.add_point((28, 19))
     s4.add_point((28, 6))
     
-    s5 = Shape([])
+    s5 = Shape()
     s5.add_point((18, 16))
     s5.add_point((18, 19))
     s5.add_point((24, 19))
     s5.add_point((24, 16))
     
-    s6 = Shape([])
+    s6 = Shape()
     s6.add_point((28, 22))
     s6.add_point((25, 22))
     s6.add_point((25, 25))
     s6.add_point((12, 25))
     s6.add_point((12, 28))
     s6.add_point((28, 28))
+    
+    shapes = [s1, s2, s3, s4, s5, s6]
     
     #Generate the graph
     
